319a455dd66ada874ac81a6960fcfe71ddd6b2e3
rohitgit0987 <rohitgit0987@gmail.com>
2025-02-05 08:41:01 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -93,6 +93,6 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "❤️" * completed_length + "🤍" * remaining_length
             progress_bar = "◆" * completed_length + "◇" * remaining_length
             try:
-                await reply.edit(f'<b>\n ╭─⌯══⟰ 𝐔𝐩𝐥𝐨𝐝𝐢𝐧𝐠 ⟰══⌯──★ \n├⚡ {progress_bar}|﹝{perc}﹞\n├⚡ {progress_bar}|﹝{perc}﹞\n├⚡ {progress_bar}|﹝{perc}﹞\n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├𝐁𝐲 » 𝐖𝐃 𝐙𝐎𝐍𝐄\n╰─══ ✪ @Opleech_WD ✪ ══─★\n</b>') 
+                await reply.edit(f'<b>\n ╭─⌯══⟰ 𝐔𝐩𝐥𝐨𝐝𝐢𝐧𝐠 ⟰══⌯──★ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├⚡ {progress_bar}|﹝{perc}﹞\n├📟 Processed » {cur}\n├⚡ {progress_bar}|﹝{perc}﹞\n├🧲 Size - ETA » {tot} - {eta} \n├⚡ {progress_bar}|﹝{perc}﹞\n├𝐁𝐲 » STRANGER BOYS\n╰─══ ✪ @Strangerboy27_strangerboy ✪ ══─★\n</b>') 
             except FloodWait as e:
                 time.sleep(e.x)
